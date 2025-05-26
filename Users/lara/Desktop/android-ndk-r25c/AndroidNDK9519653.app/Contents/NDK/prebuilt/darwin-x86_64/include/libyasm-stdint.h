0e3af60101615063eb911e2cf7b526cc78799a04
Laraozturk <laraoztrrrk@gmail.com>
2025-05-26 21:55:18 +0300

    Remove large NDK files and update gitignore
    Former-commit-id: 73081ac97f7a85cb8c9ba054a1ea587cc6c3f1fa

--- Users/lara/Desktop/android-ndk-r25c/AndroidNDK9519653.app/Contents/NDK/prebuilt/darwin-x86_64/include/libyasm-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YASM_LIBYASM_STDINT_H
-#define _YASM_LIBYASM_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "yasm 1.3.0"
-/* generated using /Volumes/Android/buildbot/src/android/ndk-r25-release/prebuilts/clang/host/darwin-x86/clang-r450784d/bin/clang --target=x86_64-apple-darwin -mmacosx-version-min=10.9 -DMACOSX_DEPLOYMENT_TARGET=10.9 -isysroot/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -Wl,-syslibroot,/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mlinker-version=711 -Os -fomit-frame-pointer -w -s */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
